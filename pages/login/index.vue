6f44e1bc7c47c6ec55599c088a15aafc811e0e54
Etcetera <yilonglei790@gmail.com>
2022-12-18 22:54:43 +0800

    新建空白布局

---
+++ pages/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">login</div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'empty-layout'
+})
+</script>
+
+<style lang="scss">
+.login {
+}
+</style>
---
+++ pages/register/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="register">register</div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'empty-layout'
+})
+</script>
+
+<style lang="scss">
+.register {
+}
+</style>
