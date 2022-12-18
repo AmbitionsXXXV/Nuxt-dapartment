432bf61adab4a750a3fdc8288ed2d39997f0ce8a
Etcetera <yilonglei790@gmail.com>
2022-12-18 22:43:11 +0800

    app-header组件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['normalize.css', '@/assets/css/global.scss'],
+  css: [
+    'normalize.css',
+    '@/assets/css/global.scss',
+    '~/assets/cus-font/iconfont.css'
+  ],
   vite: {
     css: {
       preprocessorOptions: {
