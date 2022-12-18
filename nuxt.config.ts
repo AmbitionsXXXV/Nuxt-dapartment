fb2f4f997994b2278c63f2bb5a3d736c0854de1e
Etcetera <yilonglei790@gmail.com>
2022-12-18 22:45:18 +0800

    SEO性能优化

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,24 @@ export default defineNuxtConfig({
         }
       }
     }
+  },
+  app: {
+    head: {
+      title: '哦哦啊OPPO手机商城',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'OPPO专区，官方正品，最新最全的OPPO手机产品以及配件在线抢购！'
+        },
+        {
+          name: 'keywords',
+          content: 'OPPO商城，OPPO专区, OPPO手机，OPPO配件，OPPO, OPPO官网商城'
+        },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      noscript: [{ children: 'Javascript is required' }]
+    }
   }
 })
