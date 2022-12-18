43fe24a642e66ef70f76160063fdf748f2191861
Etcetera <yilonglei790@gmail.com>
2022-12-18 22:26:07 +0800

    默认布局

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    NuxtWelcome
-  </div>
+  <NuxtLayout>
+    <NuxtPage></NuxtPage>
+  </NuxtLayout>
 </template>
+
+<script lang="ts" setup></script>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="layout">
+    <!-- Header -->
+    <app-header></app-header>
+    <!-- 页面内容的占位 -->
+    <slot></slot>
+    <!-- Footer -->
+    <app-footer></app-footer>
+  </div>
+</template>
