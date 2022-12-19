18d856810434b2ba114f518f2a7500eb14c8d91d
Etcetera <yilonglei790@gmail.com>
2022-12-19 11:15:30 +0800

    主页引入轮播图

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <div class="wrapper content">
+      <swiper :banners="banners"></swiper>
+      <tab-category
+        :categorys="categorys"
+        @item-click="handleItemClick"
+      ></tab-category>
+      <section-title title="Find N 系列"></section-title>
+      <grid-view :productDetailss="categorys![1].productDetailss"></grid-view>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import { useHomeStore } from '@/store'
+import type { ICategory } from '@/store'
+
+const homeStore = useHomeStore()
+const { banners, categorys } = storeToRefs(homeStore)
+
+function handleItemClick(item: ICategory) {
+  console.log(item.title)
+}
+</script>
+
+<style lang="scss">
+.home {
+  background-color: $bgGrayColor;
+}
+</style>
