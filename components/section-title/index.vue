5e7bbcdd12edfd1ca274d5a1831d524311d22cb2
Etcetera <yilonglei790@gmail.com>
2022-12-19 12:33:37 +0800

    分类标题组件

---
+++ components/section-title/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="section-title">{{ title }}</div>
+</template>
+<script lang="ts" setup>
+export interface IProps {
+  title: string;
+}
+const props = withDefaults(defineProps<IProps>(), {
+  title: "",
+});
+</script>
+
+<style lang="scss" scoped>
+.section-title {
+  padding-top: 60px;
+  margin-bottom: 24px;
+  font-size: 24px;
+  font-weight: 500;
+
+  /* @include border(); */
+}
+</style>
