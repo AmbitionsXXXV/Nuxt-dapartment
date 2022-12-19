45a965adfc48fe4fe86e351cfa28f9da0cbf3f95
Etcetera <yilonglei790@gmail.com>
2022-12-19 10:00:09 +0800

    配置@pinia/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,5 +32,6 @@ export default defineNuxtConfig({
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
       noscript: [{ children: 'Javascript is required' }]
     }
-  }
+  },
+  modules: ['@pinia/nuxt']
 })
